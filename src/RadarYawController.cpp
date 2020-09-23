df695de0454e4010d5b58b517f616e464668902c
mtorchalla <m.torchalla@gmail.com>
2020-09-23 18:38:42 +0200

    Update RadarYawController.cpp

--- src/RadarYawController.cpp
+++ src/RadarYawController.cpp
@@ -133,6 +133,9 @@ int main(int argc, char **argv)
   ROS_INFO("Init");
   std::string controller_cmd_topic = argv[1];  // /arm_control/radar_yaw_position_controller/command
   smooth_yaw = atoi(argv[2]);
+  ROS_INFO("%s", argv[3]);
+  ROS_INFO("%f", (float)atoi(argv[3]));
+  ROS_INFO("%f", (float)atoi(argv[4]));
   min_angle = ((205 + (float)atoi(argv[3])) * M_PI / 180);
   max_angle = ((205 + (float)atoi(argv[4])) * M_PI / 180);
   ROS_INFO("Min Angle / Max Angle: %f / %f", min_angle * 180 / M_PI, max_angle * 180 / M_PI);
